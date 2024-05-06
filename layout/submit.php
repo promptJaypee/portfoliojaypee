1f1f01e27bbb38896a2129c171a30d2d45ed1ed6
Jaypee Cabanela <jaypeedeaustriacabanela@gmail.com>
2024-05-06 14:47:49 +0800

    update as of 2:47

---
+++ layout/submit.php
@@ -0,0 +1,37 @@
+<?php
+$database_host = "localhost";
+$database_user = "root";
+$database_pass = "";
+$database_name = "portfoliojaypee";
+
+$conn = mysqli_connect($database_host, $database_user, $database_pass, $database_name);
+// Evaluate the connection
+if (mysqli_connect_errno()) {
+    die("Failed to connect with MySQL: ". mysqli_connect_error());  
+    echo mysqli_connect_error();
+    exit(); 
+}  
+
+// Set parameters
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Prepare and bind
+
+$stmt = $conn->prepare("INSERT INTO inquiry (firstname, lastname, email, message) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $firstname, $lastname, $email, $message);
+
+// Execute
+if ($stmt->execute()) {
+    echo "<script>alert('Thank you for messaging! We'll get back to you shortly!');</script>";
+    echo "<script>window.location.href='../src/index.html';</script>";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+
+?>
